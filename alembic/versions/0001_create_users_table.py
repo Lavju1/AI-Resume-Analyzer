de94a132b2bed16724cca8fe57714bfa4ee0447e
Lavju1 <lavnish70890@gmail.com>
2026-06-27 13:24:05 +0530

    Apply isort formatting to Alembic files

--- alembic/versions/0001_create_users_table.py
+++ alembic/versions/0001_create_users_table.py
@@ -7,8 +7,8 @@ Create Date: 2026-06-27 00:00:00.000000
 
 from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 revision: str = "0001_create_users_table"
